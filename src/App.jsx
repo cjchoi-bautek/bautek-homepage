f067e560eb63029063f50627953c147c755aa5a8
cjchoi-bautek <cj.choi@greenbautek.com>
2025-08-12 11:33:42 +0900

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -43,14 +43,14 @@ function AppLayout() {
                 <KeyClient />
               </section>
 
-              {/* 진행 현장 (xlsx 자동 로드 사용 예시) */}
+              //{/* 진행 현장 (xlsx 자동 로드 사용 예시) */}
               //<RunningProjectsFromXLSX
 			    //src="/data/sites.xlsx"
                 //height="70vh"
                 //lockZoom={true}
 				//lockDrag={true}
                 
-              ///>
+             
 
               <section className="min-h-[100dvh] md:h-screen md:snap-start bg-[#F4F4F4]">
                 <MapSection />
